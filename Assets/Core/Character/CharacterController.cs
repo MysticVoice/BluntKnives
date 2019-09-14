d4962b9dbc3bccdfb41cd64c2b2dbadabccadb81
MysticVoice <Fredrimv@hotmail.com>
2019-09-14 14:06:12 +0200

    Setting up HDRP and adding a character controller script

---
+++ Assets/Core/Character/CharacterController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float movementSpeed = 1;
+    public float turnSpeed = 1;
+    public float jumpForce = 1;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        walk();
+        turn();
+        jump();
+    }
+
+    void turn()
+    {
+        rb.AddTorque(Input.GetAxis("Horizontal") * transform.up * turnSpeed, ForceMode.VelocityChange);
+    }
+
+    void walk()
+    {
+        rb.AddForce(Input.GetAxis("Vertical") * transform.forward * movementSpeed, ForceMode.VelocityChange);
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(transform.up * jumpForce,ForceMode.Impulse);
+        }
+    }
+}
