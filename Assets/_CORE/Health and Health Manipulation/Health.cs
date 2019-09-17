0e8a3a8444abfd6dc7d5ca1c0d6224bdfbd5b3d8
eriknygr <eriknygr@outlook.com>
2019-09-17 17:06:38 +0200

    Health and Health Manipulation added

---
+++ Assets/_CORE/Health and Health Manipulation/Health.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private int _H, _maxH; //health and max health
+
+    Health(int maxH)    //when the Health class is called for the first time in a context you will eider provide both Current health and maxHealth or just maxHealth which will defoult Health to maxHealth
+    {
+
+        _H = maxH;
+        _maxH = maxH;
+
+    }
+
+    Health(int H, int maxH)
+    {
+
+        if (H <= maxH)
+        {
+
+            _H = H;
+            _maxH = maxH;
+
+        }
+
+        else
+        {
+
+            _H = maxH;
+            _maxH = maxH;
+
+        }
+
+    }
+
+    int getHealth() //self explanatory
+    {
+        return _H;
+    }
+    int getMaxHealth() //if you wuld ever need it
+    {
+        return _maxH;
+    }
+
+    int setHealth(int H) //self explanatory
+    {
+
+        if (H <= _maxH)
+        {
+
+            _H = H;
+
+        }
+
+        else
+        {
+
+            _H = _maxH;
+
+        }
+
+        return 0;
+    }
+
+    int setMaxHealth(int maxH) //for level-ups and sutch
+    {
+        _maxH = maxH;
+        return 0;
+    }
+
+    bool alive() // quick way to check if you are alive
+    {
+
+        if (getHealth() > 0)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+
+    }
+
+    int Heal(int H)
+    {
+
+        setHealth(getHealth() + H);
+
+        return 0;
+    }
+
+    int Damage(int D)
+    {
+
+        setHealth(getHealth() - D);
+
+        return 0;
+    }
+
+}
